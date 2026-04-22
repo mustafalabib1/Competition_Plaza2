db8430864cb97f3688208698efdebb200c1de50c
mustafalabibl <mostafalabib2004@gmail.com>
2026-04-22 19:30:37 +0200

    Refactor sensor initialization and improve command handling in training logic

--- src/AngleController.cpp
+++ src/AngleController.cpp
@@ -1,53 +1,46 @@
-#include "esp32-hal.h"
 #include "AngleController.h"
 
 // Non-blocking delays for better performance
 NonBlockingDelay rotationDelay;
 
-void rotateDegrees(int degrees) {
-  int direction = (degrees < 0) ? -1 : 1;
-  float arcLength = PI * TRACK_WIDTH * (abs(degrees * 1.00) / 360.0);
-  float wheelCircumference = PI * WHEEL_DIAMETER;
-  float revolutions = arcLength / wheelCircumference;
-  float timeSeconds = (revolutions * 60.0) / (MOTOR_RPM * robotState.baseSpeed / 255.0);
-  int timeMillis = (int)(timeSeconds * 1000 * robotState.rotationCalibration);
+void rotateDegrees(int degrees)
+{
+    int direction = (degrees < 0) ? -1 : 1;
+    float arcLength = PI * TRACK_WIDTH * (abs(degrees*1.00) / 360.0);
+    float wheelCircumference = PI * WHEEL_DIAMETER;
+    float revolutions = arcLength / wheelCircumference;
+    float timeSeconds = (revolutions * 60.0) / (MOTOR_RPM * robotState.baseSpeed / 255.0);
+    int timeMillis = (int)(timeSeconds * 1000 * robotState.rotationCalibration);
 
-  moveCar(direction * robotState.baseSpeed, -direction * robotState.baseSpeed);
-  rotationDelay.start(timeMillis);
+    moveCar(direction * robotState.baseSpeed, -direction * robotState.baseSpeed);
+    rotationDelay.start(timeMillis);
 
-  // while (!isRotationComplete())
-  // {
-  //     // Wait for rotation to complete
-  // }
+    // while (!isRotationComplete())
+    // {
+    //     // Wait for rotation to complete
+    // }
 }
-bool isRotationComplete() {
-  if (rotationDelay.isFinished()) {
-    moveCar(0, 0);  // Stop
+bool isRotationComplete()
+{
+  if (rotationDelay.isFinished())
+  {
+    moveCar(0, 0); // Stop
     return true;
   }
   return false;
 }
 
-void Right90() {
-  unsigned long pretime = millis();
-
-  while (millis() - pretime <= 1000) {
-    moveCar(230, 60);
-  }
-  moveCar(0, 0);
+void Right90()
+{
+    rotateDegrees(90);
 }
 
-// rotateDegrees(90);
-void Left90() {
-  unsigned long pretime = millis();
-  while (millis() - pretime <= 1000) {
-    moveCar(60, 230);
-  }
-  moveCar(0, 0);
-
-  // rotateDegrees(-90);
+void Left90()
+{
+    rotateDegrees(-90);
 }
 
-void UTurn() {
-  rotateDegrees(180);
+void UTurn()
+{
+    rotateDegrees(180);
 }
